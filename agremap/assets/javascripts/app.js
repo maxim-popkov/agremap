e44c19e32a7993f21d9b4c4965ccd1b15fc7eb22
maxim-popkov <maxim.popkov@outlook.com>
2016-01-31 04:13:59 +0300

    add ajax to org form

--- agremap/assets/javascripts/app.js
+++ agremap/assets/javascripts/app.js
@@ -1,5 +1,27 @@
-$( document ).ready(function() {
-  
-  console.log('We Are Ready');
+$(document).ready(function () {
+    'use strict';
+    console.log('We Are Ready');
+
+    function add_organization(target_form){
+        var frm = $(target_form);
+        $.ajax({
+            type: frm.attr('method'),
+            url: frm.attr('action'),
+            data: frm.serialize(),
+            success: function (data) {
+                frm.find('.organization-add-form__btn-add').hide();
+                frm.find('.organization-add-form__btn-close').show();
+            },
+            error: function(data) {
+                console.log(data);
+            }
+        });
+    }
+
+    $('.organization-add-form').on('submit', function (event) {
+        event.preventDefault();
+        console.log('form submit routine');
+        add_organization(event.target);
+    });
 
 });
