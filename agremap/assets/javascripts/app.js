2481fabb4444812f745705f4575c0fc8746957bb
maxim-popkov <maxim.popkov@outlook.com>
2016-01-31 04:37:37 +0300

    reset add form after approve and close

--- agremap/assets/javascripts/app.js
+++ agremap/assets/javascripts/app.js
@@ -18,10 +18,29 @@ $(document).ready(function () {
         });
     }
 
+    //events
     $('.organization-add-form').on('submit', function (event) {
         event.preventDefault();
         console.log('form submit routine');
         add_organization(event.target);
     });
+
+    $('.modal').on('hidden.bs.modal', function (e) {
+        console.log('form closed');
+        var formApproved = $('.organization-add-form__btn-add').css('display') == 'none';
+        if (!formApproved) {
+            return;
+        }
+
+        $(this)
+            .find('input,textarea,select')
+               .val('').end()
+            .find('input[type=checkbox], input[type=radio]')
+               .prop("checked", "").end()
+            .find('.organization-add-form__btn-add')
+                .show().end()
+            .find('.organization-add-form__btn-close')
+                .hide().end();
+    })
 
 });
